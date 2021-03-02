301f392a51ed47279a2dae6155c9660abab7838a
Bradley Lopez <bradleylopezherrera@gmail.com>
2021-03-02 15:02:46 -0800

    imported neccesary packages to index.js

---
+++ frontend/showpage-client/src/index.js
@@ -0,0 +1,7 @@
+// import React, ReactDOM, { createStore }, { Provider }, thunk
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
