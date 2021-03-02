e4e3062a5886780a14e7dfde8ea18301b3847d96
Bradley Lopez <bradleylopezherrera@gmail.com>
2021-03-02 15:13:13 -0800

    imported devtools extension and renamed it

--- frontend/showpage-client/src/index.js
+++ frontend/showpage-client/src/index.js
@@ -5,4 +5,4 @@ import ReactDOM from 'react-dom'
 import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
-import { composeWithDevTools } from 'redux-devtools-extension' // Use "composedWithDevTools" instead of using -> window.__Redux_devtools
+import { composeWithDevTools } from 'redux-devtools-extension' // Use "composeWithDevTools" instead of using -> window.__Redux_devtools
