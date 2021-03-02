152c022329bb3d1b27767c5812dd93ebcce58051
Bradley Lopez <bradleylopezherrera@gmail.com>
2021-03-02 15:07:25 -0800

    createStore & 'applyMiddleware'

--- frontend/showpage-client/src/index.js
+++ frontend/showpage-client/src/index.js
@@ -2,6 +2,6 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
