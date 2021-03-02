58da50152c7e25c87073ba6669a1f17d4a4af93f
Bradley Lopez <bradleylopezherrera@gmail.com>
2021-03-02 15:36:52 -0800

    created store variable for redux containing 'rootReducer' and dev tools

--- frontend/showpage-client/src/index.js
+++ frontend/showpage-client/src/index.js
@@ -8,7 +8,12 @@ import thunk from 'redux-thunk'
 import { composeWithDevtools } from 'redux-devtools-extension' // Use "composeWithDevTools" instead of using -> window.__Redux_devtools
 import App from './components/App'
 
+// Create a store for redux => createStore 1. reducer 2. dev tools
 
+const store = createStore(
+    rootReducer, 
+    composeWithDevtools(applyMiddleware(thunk))
+)
 
 
 
