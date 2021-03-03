d04334f20beba0ba19b92a6a56a2ed22f245e129
Bradley Lopez <bradleylopezherrera@gmail.com>
2021-03-02 17:35:51 -0800

    new user form component

---
+++ frontend/showpage-client/src/components/UsersForm.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class UsersForm extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
