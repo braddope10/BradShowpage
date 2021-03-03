12ed2710f44fbba4c21c6db81e6af960c3023db1
Bradley Lopez <bradleylopezherrera@gmail.com>
2021-03-02 16:46:13 -0800

    fixed reducers problem, combined reducers option, and spellchecks

--- frontend/showpage-client/src/index.js
+++ frontend/showpage-client/src/index.js
@@ -2,23 +2,23 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {createStore, applyMiddleware, compose } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
-import { composeWithDevtools } from 'redux-devtools-extension' // Use "composeWithDevTools" instead of using -> window.__Redux_devtools
+import { composeWithDevTools } from 'redux-devtools-extension' // Use "composeWithDevTools" instead of using -> window.__Redux_devtools
 import App from './components/App'
-import { usersReducer } from './reducers/usersReducer'
+import { rootReducer } from './reducers/rootReducer'
 
 // Create a store for redux => createStore 1. reducer 2. dev tools
 
 const store = createStore(
-    usersReducer, 
-    composeWithDevtools(applyMiddleware(thunk))
-)
+    rootReducer, 
+    composeWithDevTools(applyMiddleware(thunk))
+);
 
 ReactDOM.render(
     <Provider store={store}>
         <App />
     </Provider>,
     document.getElementById('root')
-)
+);
