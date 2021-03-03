2cac8184b36d97db567919eed3c872b6131bc7c5
Bradley Lopez <bradleylopezherrera@gmail.com>
2021-03-02 17:09:05 -0800

    displaying UsersContainer

--- frontend/showpage-client/src/components/App.js
+++ frontend/showpage-client/src/components/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import UsersContainer from './UsersContainer'
 
 const App = () => {
     return (
         <div>
-            Hello World!
+            <UsersContainer />
         </div>
     );
 };
---
+++ frontend/showpage-client/src/components/UsersContainer.js
@@ -0,0 +1,13 @@
+// This will be our container = data + methods
+
+import React, { Component } from 'react'
+
+export default class UsersContainer extends Component {
+    render() {
+        return (
+            <div>
+                Users Container
+            </div>
+        )
+    }
+}
