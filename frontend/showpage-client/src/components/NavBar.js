3f9af266a582d1354d8cea787b159b51b26a88d4
Bradley Lopez <bradleylopezherrera@gmail.com>
2021-03-02 21:12:33 -0800

    created basic NavBar

---
+++ frontend/showpage-client/src/components/NavBar.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import './App.css'
+
+class NavBar extends Compenent {
+    render() {
+        return (
+            <nav>
+                <Link to="/">
+                    <h3>About Me</h3>
+                </Link>
+
+                <ul className="nav-links">
+                    <Link to="/projects">
+                        <li>Projects</li>
+                    </Link>
+
+                    <Link to="/technologies">
+                        <li>Technologies</li>
+                    </Link>
+
+                    <Link to="/contact">
+                        <li>Contact</li>
+                    </Link>
+
+                    <Link to="/messageboard">
+                        <li>Messages Board</li>
+                    </Link>
+
+                </ul>
+            </nav>
+        )
+    }
+}
+
+export default NavBar
