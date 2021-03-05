39d5a879b13a7a49daed4c72b53581b3ff0de9e1
Bradley Lopez <bradleylopezherrera@gmail.com>
2021-03-04 17:24:21 -0800

    completed slider data image transfer

--- frontend/showpage-client/src/slider/SliderData.js
+++ frontend/showpage-client/src/slider/SliderData.js
@@ -4,75 +4,103 @@ export const SliderData = [
         alt: 'image one'
     },
     {
-        image: './images/night-2',
+        image: 'https://github.com/braddope10/BradShowpage/blob/main/frontend/showpage-client/src/slider/images/night-2.png?raw=true',
         alt: 'image one'
     },
     {
-        image: './images/night-3',
+        image: 'https://github.com/braddope10/BradShowpage/blob/main/frontend/showpage-client/src/slider/images/night-3.png?raw=true',
         alt: 'image one'
     },
     {
-        image: './images/night-4',
+        image: 'https://github.com/braddope10/BradShowpage/blob/main/frontend/showpage-client/src/slider/images/night-5.png?raw=true',
         alt: 'image one'
     },
     {
-        image: './images/night-5',
+        image: 'https://github.com/braddope10/BradShowpage/blob/main/frontend/showpage-client/src/slider/images/personal-1.png?raw=true',
         alt: 'image one'
     },
     {
-        image: './images/personal-1',
+        image: 'https://github.com/braddope10/BradShowpage/blob/main/frontend/showpage-client/src/slider/images/personal-2.png?raw=true',
         alt: 'image one'
     },
     {
-        image: './images/personal-2',
+        image: 'https://github.com/braddope10/BradShowpage/blob/main/frontend/showpage-client/src/slider/images/personal-3.png?raw=true',
         alt: 'image one'
     },
     {
-        image: './images/personal-3',
+        image: 'https://github.com/braddope10/BradShowpage/blob/main/frontend/showpage-client/src/slider/images/personal-4.JPG?raw=true',
         alt: 'image one'
     },
     {
-        image: './images/personal-4',
+        image: 'https://github.com/braddope10/BradShowpage/blob/main/frontend/showpage-client/src/slider/images/personal-4.jpeg?raw=true',
         alt: 'image one'
     },
     {
-        image: './images/personal-5',
+        image: 'https://github.com/braddope10/BradShowpage/blob/main/frontend/showpage-client/src/slider/images/personal-5.jpeg?raw=true',
         alt: 'image one'
     },
     {
-        image: './images/personal-6',
+        image: 'https://github.com/braddope10/BradShowpage/blob/main/frontend/showpage-client/src/slider/images/personal-5.png?raw=true',
         alt: 'image one'
     },
     {
-        image: './images/personal-7',
+        image: 'https://github.com/braddope10/BradShowpage/blob/main/frontend/showpage-client/src/slider/images/personal-6.jpeg?raw=true',
         alt: 'image one'
     },
     {
-        image: './images/salt-1',
+        image: 'https://github.com/braddope10/BradShowpage/blob/main/frontend/showpage-client/src/slider/images/personal-7.jpeg?raw=true',
         alt: 'image one'
     },
     {
-        image: './images/salt-2',
+        image: 'https://github.com/braddope10/BradShowpage/blob/main/frontend/showpage-client/src/slider/images/salt-1.png?raw=true',
         alt: 'image one'
     },
     {
-        image: './images/salt-3',
+        image: 'https://github.com/braddope10/BradShowpage/blob/main/frontend/showpage-client/src/slider/images/salt-2.JPG?raw=true',
         alt: 'image one'
     },
     {
-        image: './images/salt-4',
+        image: 'https://github.com/braddope10/BradShowpage/blob/main/frontend/showpage-client/src/slider/images/salt-3.png?raw=true',
         alt: 'image one'
     },
     {
-        image: './images/salt-5',
+        image: 'https://github.com/braddope10/BradShowpage/blob/main/frontend/showpage-client/src/slider/images/salt-4.JPG?raw=true',
         alt: 'image one'
     },
     {
-        image: './images/salt-6',
+        image: 'https://github.com/braddope10/BradShowpage/blob/main/frontend/showpage-client/src/slider/images/salt-5.png?raw=true',
         alt: 'image one'
     },
     {
-        image: './images/salt-7',
+        image: 'https://github.com/braddope10/BradShowpage/blob/main/frontend/showpage-client/src/slider/images/salt-6.JPG?raw=true',
+        alt: 'image one'
+    },
+    {
+        image: 'https://github.com/braddope10/BradShowpage/blob/main/frontend/showpage-client/src/slider/images/salt-7.png?raw=true',
+        alt: 'image one'
+    },
+    {
+        image: 'https://github.com/braddope10/BradShowpage/blob/main/frontend/showpage-client/src/slider/images/salt-8.jpeg?raw=true',
+        alt: 'image one'
+    },
+    {
+        image: 'https://github.com/braddope10/BradShowpage/blob/main/frontend/showpage-client/src/slider/images/salt-9.jpeg?raw=true',
+        alt: 'image one'
+    },
+    {
+        image: 'https://github.com/braddope10/BradShowpage/blob/main/frontend/showpage-client/src/slider/images/salt-10.jpeg?raw=true',
+        alt: 'image one'
+    },
+    {
+        image: 'https://github.com/braddope10/BradShowpage/blob/main/frontend/showpage-client/src/slider/images/salt-11.jpeg?raw=true',
+        alt: 'image one'
+    },
+    {
+        image: 'https://github.com/braddope10/BradShowpage/blob/main/frontend/showpage-client/src/slider/images/salt-12.jpeg?raw=true',
+        alt: 'image one'
+    },
+    {
+        image: 'https://github.com/braddope10/BradShowpage/blob/main/frontend/showpage-client/src/slider/images/salt-13.jpeg?raw=true',
         alt: 'image one'
     },
     {
@@ -80,87 +108,79 @@ export const SliderData = [
         alt: 'image one'
     },
     {
-        image: './images/fam-2',
+        image: 'https://github.com/braddope10/BradShowpage/blob/main/frontend/showpage-client/src/slider/images/fam-2.png?raw=true',
         alt: 'image one'
     },
     {
-        image: './images/fam-3',
+        image: 'https://github.com/braddope10/BradShowpage/blob/main/frontend/showpage-client/src/slider/images/fam-3.png?raw=true',
         alt: 'image one'
     },
     {
-        image: './images/fam-4',
+        image: 'https://github.com/braddope10/BradShowpage/blob/main/frontend/showpage-client/src/slider/images/fam-4.png?raw=true',
         alt: 'image one'
     },
     {
-        image: './images/fam-5',
+        image: 'https://github.com/braddope10/BradShowpage/blob/main/frontend/showpage-client/src/slider/images/fam-5.png?raw=true',
         alt: 'image one'
     },
     {
-        image: './images/fam-6',
+        image: 'https://github.com/braddope10/BradShowpage/blob/main/frontend/showpage-client/src/slider/images/redrock-1.png?raw=true',
         alt: 'image one'
     },
     {
-        image: './images/redrock-1',
+        image: 'https://github.com/braddope10/BradShowpage/blob/main/frontend/showpage-client/src/slider/images/redrock-2.JPG?raw=true',
         alt: 'image one'
     },
     {
-        image: './images/redrock-2',
+        image: 'https://github.com/braddope10/BradShowpage/blob/main/frontend/showpage-client/src/slider/images/lulu-2.png?raw=true',
         alt: 'image one'
     },
     {
-        image: './images/lulu-1',
+        image: 'https://github.com/braddope10/BradShowpage/blob/main/frontend/showpage-client/src/slider/images/lulu-3.png?raw=true',
         alt: 'image one'
     },
     {
-        image: './images/lulu-2',
+        image: 'https://github.com/braddope10/BradShowpage/blob/main/frontend/showpage-client/src/slider/images/lulu-4.png?raw=true',
         alt: 'image one'
     },
     {
-        image: './images/lulu-3',
+        image: 'https://github.com/braddope10/BradShowpage/blob/main/frontend/showpage-client/src/slider/images/lulu-5.png?raw=true',
         alt: 'image one'
     },
     {
-        image: './images/lulu-4',
+        image: 'https://github.com/braddope10/BradShowpage/blob/main/frontend/showpage-client/src/slider/images/zoo-1.png?raw=true',
         alt: 'image one'
     },
     {
-        image: './images/lulu-5',
+        image: 'https://github.com/braddope10/BradShowpage/blob/main/frontend/showpage-client/src/slider/images/zoo-2.png?raw=true',
         alt: 'image one'
     },
     {
-        image: './images/zoo-1',
+        image: 'https://github.com/braddope10/BradShowpage/blob/main/frontend/showpage-client/src/slider/images/zoo-3.png?raw=true',
         alt: 'image one'
     },
     {
-        image: './images/zoo-2',
+        image: 'https://github.com/braddope10/BradShowpage/blob/main/frontend/showpage-client/src/slider/images/zoo-4.png?raw=true',
         alt: 'image one'
     },
     {
-        image: './images/zoo-3',
+        image: 'https://github.com/braddope10/BradShowpage/blob/main/frontend/showpage-client/src/slider/images/zoo-5.png?raw=true',
         alt: 'image one'
     },
     {
-        image: './images/zoo-4',
+        image: 'https://github.com/braddope10/BradShowpage/blob/main/frontend/showpage-client/src/slider/images/stuff-1.png?raw=true',
         alt: 'image one'
     },
     {
-        image: './images/zoo-5',
+        image: 'https://github.com/braddope10/BradShowpage/blob/main/frontend/showpage-client/src/slider/images/stuff-2.png?raw=true',
         alt: 'image one'
     },
     {
-        image: './images/stuff-1',
+        image: 'https://github.com/braddope10/BradShowpage/blob/main/frontend/showpage-client/src/slider/images/stuff-3.png?raw=true',
         alt: 'image one'
     },
     {
-        image: './images/stuff-2',
-        alt: 'image one'
-    },
-    {
-        image: './images/stuff-3',
-        alt: 'image one'
-    },
-    {
-        image: './images/stuff-4',
+        image: 'https://github.com/braddope10/BradShowpage/blob/main/frontend/showpage-client/src/slider/images/stuff-4.png?raw=true',
         alt: 'image one'
     },
     
