b2b99aa2b96b03a7c29fbe69cc3b6433c752940e
Bradley Lopez <bradleylopezherrera@gmail.com>
2021-04-15 00:45:42 -0700

    configuring backend

--- frontend/showpage-client/src/components/stateless/Projects.js
+++ frontend/showpage-client/src/components/stateless/Projects.js
@@ -19,14 +19,74 @@ const Projects = () => {
                 <span>!</span>
             </h1>
             <div className="tiny-link">
+
                 <ReactTinyLink
-                    cardSize="large"
+                    cardSize="small"
                     showGraphic={true}
-                    header="Hotel Registry App (SQL, Ruby, RoR, JavaScript, React, Redux)"
-                    description="not much"
-                    maxLine={2}
+                    header="Personal Showpage (SQL, Ruby, Rails, JavaScript, React, Redux)"
+                    description="A website where you will be introduced to my personal and professional life!"
+                    maxLine={5}
+                    minLine={1}
+                    url="https://github.com/braddope10/BradShowpage"
+                />
+
+                <br />
+                
+                <ReactTinyLink
+                    cardSize="small"
+                    showGraphic={true}
+                    header="Hotel Registry App (SQL, Ruby, Rails, JavaScript, React, Redux)"
+                    description="Register new guests. Create & update posts pertaining to room status."
+                    maxLine={5}
                     minLine={1}
                     url="https://github.com/braddope10/reactReduxApp"
+                />
+
+                <br />
+
+                <ReactTinyLink
+                    cardSize="small"
+                    showGraphic={true}
+                    header="Zodiac Sign Calculator (SQL, Ruby, Rails, JavaScript)"
+                    description="An application that requires you to fill out a form in order for you to see you Zodiac sign."
+                    maxLine={5}
+                    minLine={1}
+                    url="https://github.com/braddope10/zodiac_javascript"
+                />
+
+                <br />
+
+                <ReactTinyLink
+                    cardSize="small"
+                    showGraphic={true}
+                    header="Truck Driver App (SQL, Ruby, Rails)"
+                    description="An application that can be used as a tool by truckers around the nation to create a detailed record of their varying day-to-day shifts."
+                    maxLine={5}
+                    minLine={1}
+                    url="https://github.com/braddope10/my_trucking"
+                />
+
+                <br />
+
+                <ReactTinyLink
+                    cardSize="small"
+                    showGraphic={true}
+                    header="Pokemon Collector (SQL, Ruby, Sinatra)"
+                    description="Allows pokemon card collectors to virtualize their collection while also giving them a chance to see other great collections."
+                    maxLine={5}
+                    minLine={1}
+                    url="https://github.com/braddope10/sinatra-project"
+                />
+                <br />
+
+                <ReactTinyLink
+                    cardSize="small"
+                    showGraphic={true}
+                    header="Nevada City Population CLI (Ruby, Nokogiri)"
+                    description="A gem that you interact with, using the terminal, to see the populations of different cities in Nevada."
+                    maxLine={5}
+                    minLine={1}
+                    url="https://github.com/braddope10/city_pop"
                 />
             </div>
             <QuoteTwo />
