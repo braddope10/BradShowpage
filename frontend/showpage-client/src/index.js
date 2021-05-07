1479a8b9b359a06b2b7d9ddfe86f9b285feac9cb
Bradley Lopez <bradleylopezherrera@gmail.com>
2021-05-06 20:54:57 -0700

    removed redux and any backend middleware

--- frontend/showpage-client/src/index.js
+++ frontend/showpage-client/src/index.js
@@ -2,27 +2,15 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { createStore, applyMiddleware} from 'redux'
-import { Provider } from 'react-redux'
-import thunk from 'redux-thunk'
-import { composeWithDevTools } from 'redux-devtools-extension' // Use "composeWithDevTools" instead of using -> window.__Redux_devtools
+
 import App from './components/App'
-import { rootReducer } from './reducers/rootReducer'
 import { BrowserRouter as Router } from 'react-router-dom'
 import './index.css'
 
-// Create a store for redux => createStore 1. reducer 2. dev tools
-
-const store = createStore(
-    rootReducer, 
-    composeWithDevTools(applyMiddleware(thunk))
-);
 
 ReactDOM.render(
-    <Provider store={store}>
-        <Router>
-            <App />
-        </Router>
-    </Provider>,
+    <Router>
+        <App />
+    </Router>,
     document.getElementById('root')
 );
