cf29cfa1794b0bb7d178177e19f615c42d2acd2a
Bradley Lopez <bradleylopezherrera@gmail.com>
2022-05-16 20:33:59 -0700

    new update

--- src/components/AboutMeParagraphs/ParagraphTwo.js
+++ src/components/AboutMeParagraphs/ParagraphTwo.js
@@ -1,19 +1,19 @@
-import '../Internal.css'
-
+import "../Internal.css";
 
 const ParagraphTwo = () => {
-    return (
-        <p className='classic-about'>
-            My name is <strong>Bradley Lopez</strong>, 
-            I am a detail-oriented college student currently attending College of Southern Nevada
-            in Las Vegas for <strong>Computing & Information Technology</strong> where
-            I am in the process of obtaining an associates degree. 
-            I’ve proudly been able to obtain a certificate in <strong>Full-Stack Software Engineering</strong> through
-            a 600+ hour course at Flatiron School. 
-            My long term goal is to work toward a Bachelor’s degree in <strong>Computer Science</strong>, 
-            and I’m eager to fulfill that achievement in the near future.
-        </p>
-    )
-}
+  return (
+    <p className="classic-about">
+      My name is <strong>Bradley Lopez</strong>, I am a detail-oriented college
+      student currently attending University of Nevada, Las Vegas for{" "}
+      <strong>Computer Science</strong> where I am in the process of obtaining
+      an Bachelor of Science degree. I’ve proudly been able to attain a
+      certificate in <strong>Full-Stack Software Engineering</strong> through a
+      600+ hour course at Flatiron School. My long term goal is to work toward a
+      Bachelor’s degree in <strong>Computer Science</strong> and build on that
+      momentum in a well established company, and I’m eager to fulfill that
+      achievement in the near future.
+    </p>
+  );
+};
 
-export default ParagraphTwo
+export default ParagraphTwo;
